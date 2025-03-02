bf16fcb0ab63a8da01b64405314d39fc83eda1d0
nahidkhan31 <nahidkhanniloy31@.com>
2025-03-03 04:10:28 +0600

    pb solve

---
+++ variable.js
@@ -0,0 +1,30 @@
+// problem-01
+const totalEarn = 100;
+const totalExpenditure = 40;
+const totalRemained = totalEarn - totalExpenditure;
+console.log(totalRemained);
+
+// problem-02
+const totalPencil = 10;
+const perUse = 2;
+const totalUse = totalPencil / perUse;
+console.log(totalUse);
+
+// problem-03
+const totalMoney = 60;
+const perExpenditure = 15;
+const totalPerExpenditure = totalMoney / perExpenditure;
+const totalRemainderMoney = totalMoney % perExpenditure;
+console.log(totalPerExpenditure, totalRemainderMoney);
+
+// problem-04
+const first = "Hello"
+const second = "World"
+const sum = first + second;
+console.log(sum);
+
+// problem-05
+const rice = 153;
+const dal = 261;
+const totalKg = rice + dal;
+console.log(totalKg);
